be3593029d9d1ea77d3611c61abed597b270ea37
Mari Azh <Marzi.aghchay@gmail.com>
2023-04-04 12:16:53 +0100

    res.write

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const https = require("https")
 
 
 app.get("/",(req,res)=>{
-    res.send("well done")
-})
 
 const url =
   "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=5df5aaa05355ac3dbf39a3bd7ade7fa0";
-https.get(url ,(res)=>{
-    console.log(res);
+
+https.get(url, (response) => {
+  response.on("data", (data) => {
+
+    const weatherData = JSON.parse(data);
+    const temp = weatherData.main.temp;
+    const description = weatherData.weather[0].description;
+    res.write("<p>the weather currently is  " + description + "</p>")
+    res.write("<h1>the temp in London is " + temp  +"</h1>");
+    res.send()
+  });
 })
-
-
-
-
+})
 
 app.listen(3000,()=>{
     console.log("the server is running on port 3000");
