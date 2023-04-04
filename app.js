9a2f6995ed050264a5d66a2d4f54cd4cb72c5559
Mari Azh <Marzi.aghchay@gmail.com>
2023-04-04 20:59:24 +0100

    some change in URL

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const https = require("https")
 
 app.get("/",(req,res)=>{
 
+    const query = "London"
+    const apiKey = "5df5aaa05355ac3dbf39a3bd7ade7fa0";
+    const units = "metric"
+
 const url =
-  "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=5df5aaa05355ac3dbf39a3bd7ade7fa0";
+  "https://api.openweathermap.org/data/2.5/weather?q="+ query+"&units="+ units+"&appid="+ apiKey;
 
 https.get(url, (response) => {
   response.on("data", (data) => {
@@ -17,7 +21,7 @@ https.get(url, (response) => {
     const icon = weatherData.weather[0].icon;
     const imageUrl = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
     res.write("<p>the weather currently is  " + description + "</p>")
-    res.write("<h1>the temp in London is " + temp  +"</h1>");
+    res.write("<h1>the temp in London  is  " + temp  +"</h1>");
     res.write("<img src =" +imageUrl + ">")
     res.send()
   });
